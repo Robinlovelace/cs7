b9cfbbcb2ab7a68d244f34023926f6d9c7e69f40
Robin <rob00x@gmail.com>
2017-05-17 11:35:11 +0200

    Add script - reproducible

---
+++ basic-script.R
@@ -0,0 +1,39 @@
+2 + 5 * 2
+
+(2 + 5) * 2
+
+
+ls()
+
+x = 1:9
+
+sqrt(x = x)
+
+# this is a comment - use them!
+# use TAB autocomplete for arguments
+plot(x = x, main = "Some data", xlab = "xlab")
+
+# style
+plot(x=x,main ="Some data")
+# Ctl-alt-A can format - you won't need to!
+plot(x = x, main = "Some data")
+
+a <- 1
+1 -> a
+x_thingy = 1:9
+xx_thingy = x_thingy^2
+C = "C"
+# comment out broken code
+# a + C # use Ctl-Shift-C -
+# tip: use p+Ctl+UP to search history
+
+# tip
+x <- data.frame(x = 1:3, y = letters[1:3])
+x
+# only keep useful code in console - delete rest
+x$x_sqrd = x$x^2
+
+# in console: z = x$x_sqrd^3
+# plot(x$x, z)
+# only save reproducible script files
+
